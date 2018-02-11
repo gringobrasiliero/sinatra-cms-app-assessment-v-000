069fc20a2c051f936154820473b89d9d89b87f57
Nolan Byrnes <nolanehs@gmail.com>
2018-02-11 12:42:50 +0000

    Added Slugified/slug/find_by_slug-- added app_controller--enabled sessions

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 class Attribute < ActiveRecord::Base
 belongs_to :user
+=======
+class User < ActiveRecord::Base
+
+>>>>>>> 32f5c8fba581bff55ef0c5aaf9a918b2e690425d
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :email, :password
 has_secure_password
+<<<<<<< HEAD
 has_many :attributes
+=======
+>>>>>>> 32f5c8fba581bff55ef0c5aaf9a918b2e690425d
 end
